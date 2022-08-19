12feec8e091a349f1884c1ef79e27e74ac4da7be
Harrison Fried <hsfried25@students.bmchs.org>
2022-08-19 14:34:17 -0500

    1.03

---
+++ lab_1.03.py
@@ -0,0 +1,40 @@
+'''
+Lab 1.03 - Printing & Variables
+
+Part 1 - Printing Practice
+Practice typing out some statements in the editor part of the IDE, then hit "Run" at the top of the screen:
+**Remember you must exit() python interpreter before running a script!!!
+
+    Expression                                      Expected Output                               Did anything unexpected happen?
+a)  print("1")                                              1                                                    no
+b)  print(1)                                                1                                                    no
+c)  print(1 + 2)                                            1+2                                                  yes
+d)  print("1" + "2")                                        12                                                   no
+e)  print("this" + " " + "is" + " " +                this is a scentence
+    "a" + " " +
+    "sentence" + ".")
+
+Part 2 - Variables Practice
+1. In your Console
+Type and run the following:
+    animal = "dogs"
+    print(animal + " are really cool.")
+In your Notebook
+Respond to the following:
+    1. What happens?           it says dogs are really cool
+    2. How would you make the program print out "cats are really cool" instead?   istead of going animal = dogs, id say, animal = cats
+
+2. In your Console
+Type and run the following code:
+print(dogs + " are cool.")
+In your Notebook
+Respond to the following questions:
+    1. What output does this produce?  it gives an error
+    2. Why does this happen?           i did not give 'dogs' a value
+'''
+
+print("1")
+print(1)
+print(1 + 2)
+print("1" + "2")
+print("this" + " " + "is" + " " + "a" + " " +"sentence" + ".")
